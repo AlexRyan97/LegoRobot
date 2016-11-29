9258ffbf092032edff9799d186b7527c1933ce7f
Eulerdice <sabondrea1@sheffield.ac.uk>
2016-11-29 13:15:49 +0000

    Moved robot package's files to main directory

--- Assignment3.java
+++ Assignment3.java
@@ -1,10 +1,10 @@
 /* A Lego Robot Java program 
- * Written by: Alexander Ryan, Alex Bondrea, Vincent Peirong Ko 
+ * Written by: Alexander Ryan, Stefan Alexandru Bondrea, Vincent Peirong Ko 
  * First written: 29/10/2016
  * Last updated:
  */
  
- import ShefRobot.*; 
+import ShefRobot.*; 
 public class Assignment3 { 
 	
 		public static void main(String[] args) {
