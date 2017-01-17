8a5619f66c01b531c9d413e49b889217c48b5697
vincentko37 <vkprong1@sheffield.ac.uk>
2017-01-17 15:42:58 +0000

    updated twisterRobot, added in navColour method (incomplete)

--- TwisterRobot.java
+++ TwisterRobot.java
@@ -2,10 +2,11 @@ import ShefRobot.*;
 
 public class TwisterRobot {
   Robot shefRobot = new Robot();
-  Motor leftMotor = shefRobot.getLargeMotor(Motor.Port.B);
+  Motor leftMotor = shefRobot.getLargeMotor(Motor.Port.D);
   Motor rightMotor = shefRobot.getLargeMotor(Motor.Port.A);
   Speaker speaker = shefRobot.getSpeaker();
   ColorSensor colorSensor = shefRobot.getColorSensor(Sensor.Port.S1);
+  //GyroSensor gyroSensor = shefRobot.getGyroSensor();
   
   public void setSpeed(int speed)
   {
@@ -38,34 +39,73 @@ public class TwisterRobot {
   	while(colorSensor.getColor() == ColorSensor.Color.WHITE)
       moveForward();
   }
-
+/*
   public void navGrid() 
   { 
-    setSpeed(50);
+    setSpeed(100);
     if(colorSensor.getColor()!=ColorSensor.Color.WHITE) {
       System.out.println("hello i am black");
       moveForward();
       navGrid();
     }
-
-    else if (colorSensor.getColor()==ColorSensor.Color.BLUE) {
-   	  System.out.println("hello i am blue");
-   	  stop();
-    }
-   	
     else {
      	System.out.println("hello i am white 2");
-     	rotateRight(100);
-     	moveForward();
+     	rightMotor.stop();
+     	leftMotor.forward();
+     	navGrid();
+    }
+  }
+  */
+  
+   public void navGrid() 
+  { 
+    setSpeed(100);
+    if(colorSensor.getColor()!=ColorSensor.Color.WHITE) {
+      System.out.println("hello i am black");
+		 if(colorSensor.getColor()==ColorSensor.Color.BLACK) {
+		  moveForward();
+		  navGrid();
+		 }
+		 /*else {
+		  leftMotor.stop(); //break from method
+		  rightMotor.forward();
+		  navGrid();
+     }*/
+    }
+    else {
+     	System.out.println("hello i am white 2");
+     	moveBackward();
      	navGrid();
     }
   }
   
-  /*
+  public void navColour() 
+  {
+  	setSpeed(100);
+    if(colorSensor.getColor()!= ColorSensor.Color.WHITE) {
+    	if (colorSensor.getColor()!= ColorSensor.Color.BLACK){
+    		System.out.println("hello i am on a colour circle");
+    		moveForward();
+    		navColour();
+		 }
+		 else {
+		 	 System.out.println("hello i have found black");
+		 	 navGrid();
+		 }
+	}
+	else {
+		System.out.println("hello i am white 3");
+		rightMotor.stop();
+		leftMotor.forward();
+		navColour();
+		}	
+  }  
+  
+  
+ /* 
    public void navGrid() 
   { 
    setSpeed(50);
-   System.out.println(colorSensor.getColor());
    if(colorSensor.getColor()!=ColorSensor.Color.WHITE){
     moveForward(); 
    }
@@ -99,6 +139,7 @@ public class TwisterRobot {
   	TwisterRobot robo = new TwisterRobot();
   	robo.findGrid();
   	robo.navGrid();
+  	robo.navColour();
   	robo.stop();
   }
 }
